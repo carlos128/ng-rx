f8b3e6adc223f83b439d4eacc69eaa7b8df34e69
carlos128 <gcralos@ymail.com>
2019-04-01 07:54:45 -0500

    estructura de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HijoComponent } from './hijo/hijo.component';
+import { NietoComponent } from './contador/hijo/nieto/nieto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    HijoComponent,
+    NietoComponent
   ],
   imports: [
     BrowserModule,
